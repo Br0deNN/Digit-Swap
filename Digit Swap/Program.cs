c7bbae26922e575c48c1d340a90079ff6b23b412
7R1XY <theo.broden@gmail.com>
2024-01-10 10:42:20 +0100

    Add project files.

---
+++ Digit Swap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Digitswap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            char[] y = x.ToCharArray();
+
+            Array.Reverse(y);
+
+            string reversed = new string(y);
+
+            Console.WriteLine(reversed);
+
+            
+
+
+
+        }
+    }
+}
